3ef0bace66685177f6e1e95a38e8c206218f4aaa
Adriano Frota <adriano.internet@gmail.com>
2023-03-03 11:17:35 -0300

    MarvelHeroService

---
+++ DotNet7-UnitTest-Project.Tests/01 - basis/MarvelHeroServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+using DotNet7_UnitTest_Project._01___basis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace DotNet7_UnitTest_Project._01___basis.Tests
+{
+    public class MarvelHeroServiceTests
+    {
+        [Fact()]
+        public void MarvelHeroService_GetHeroAge_ReturnInt()
+        {
+            //arrange
+            string name = "Thor";
+            MarvelHeroService ms = new MarvelHeroService();
+
+            //act 
+            var result = ms.GetHeroAge(name);
+
+            //assert
+            Assert.IsType<int>(result);
+
+        }
+
+        [Fact()]
+        public void MarvelHeroService_CheckHeroIsAlive_ReturnString()
+        {
+            string name = "Thor";
+            MarvelHeroService ms = new();
+
+            //act 
+            var result = ms.CheckHeroIsAlive(name);
+
+            //assert
+            Assert.IsType<bool>(result);
+            result.Should().BeTrue();
+            result.Should().Be(true);  
+        }
+        [Fact()]
+        public void MarvelHeroService_GetHeroWeapon_ReturnString()
+        {
+            string name = "Thor";
+            string expected = "Hammer";
+            MarvelHeroService ms = new();
+
+            //act 
+            var result = ms.GetHeroWeapon(name);
+           
+            //assert
+            Assert.IsType<string>(result);
+            result.Should().StartWith("Ha").And.EndWith("er").And.Contain("mm").And.HaveLength(6);
+            result.Should().BeSameAs(expected); 
+        }
+
+    }
+}
---
+++ DotNet7-UnitTest-Project/01 - basis/MarvelHeroService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet7_UnitTest_Project._01___basis
+{
+    public class MarvelHeroService
+    {
+        public bool CheckHeroIsAlive(string heroName)
+        {
+            if(heroName == "Thor")
+            {
+                return true;
+            }
+                 return false;
+        }
+
+        public int GetHeroAge(string heroName)
+        {
+            if (heroName == "Thor")
+            {
+                return 1500;
+            }
+            return 0;
+        }
+
+        public string GetHeroWeapon(string heroName)
+        {
+            if (heroName == "Thor")
+            {
+                return "Hammer";
+            }
+            return "IDK";
+        }
+    }
+}
